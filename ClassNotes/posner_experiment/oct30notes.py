051bab417145127333df606a4ef9ab6712f5f199
ErykaJones <joneseryka0@gmail.com>
2025-10-30 19:21:55 -0400

    10/30 class notes

---
+++ ClassNotes/posner_experiment/oct30notes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 30 15:51:52 2025
+
+@author: psych
+"""
+
+#builder notes
+# builder will show the amount of time it is set to display
+#can also record key response 
+
+#to see the python script, there is a button next to the run button that will write it in a python script
+#it's heavy though because it needs to be for it to translate to javascript
+#height is the percentage of the screen it takes up, pixels is generally not recommended because it's not transferable
+#to make a condition list:
+#columns show what we want to manipulate
+#image column, orientation, different x plot, condition, correct answer 
+#num repeats refers to repeating all your conditions, so if yyou have three conditions and 5 repeats, that would be 15 trials
+
+
+#random will populate a list of n-1 of your conditions, so you will never see the same  condition twice in a row
+#full random will allow that to happen
+
+#if you want randomly orienting stimuli, you can set the orientation to every repeat
+#when importing excel, you can input the heading name into something like object orientation so it knows where to read it 
+
+#if you want it to read the text as a variable, use a $ (some spots will already have it)
+
+#custom code will translate to java too
+#code that needs to interact with stimuli should go under it because of how code is read one line at a time 
